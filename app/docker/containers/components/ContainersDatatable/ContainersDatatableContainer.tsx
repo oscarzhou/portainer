0eed6a9e4719778592bc03d8f5dfa417df44e2f2
Chaim Lev-Ari <chiptus@gmail.com>
2022-02-24 16:21:43 +0200

    refactor(containers): move data loading to a separate component

--- app/docker/containers/components/ContainersDatatable/ContainersDatatableContainer.tsx
+++ app/docker/containers/components/ContainersDatatable/ContainersDatatableContainer.tsx
@@ -1,16 +1,26 @@
 import { react2angular } from '@/react-tools/react2angular';
-import { TableSettingsProvider } from '@/portainer/components/datatables/components/useTableSettings';
+import {
+  TableSettingsProvider,
+  useTableSettings,
+} from '@/portainer/components/datatables/components/useTableSettings';
+
+import { Filters } from '../../containers.service';
+import { ContainersTableSettings } from '../../types';
+import { useContainers } from '../../queries';
 
 import {
   ContainersDatatable,
   Props as ContainerDatatableProps,
 } from './ContainersDatatable';
 
+interface Props extends Omit<ContainerDatatableProps, 'containers'> {
+  filters?: Filters;
+}
+
 export function ContainersDatatableContainer({
-  environment,
   tableKey = 'containers',
   ...props
-}: ContainerDatatableProps) {
+}: Props) {
   const defaultSettings = {
     autoRefreshRate: 0,
     truncateContainerName: 32,
@@ -23,8 +33,38 @@ export function ContainersDatatableContainer({
   return (
     <TableSettingsProvider defaults={defaultSettings} storageKey={tableKey}>
       {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-      <ContainersDatatable {...props} environment={environment} />
+      <ContainersLoader {...props} />
     </TableSettingsProvider>
+  );
+}
+
+function ContainersLoader({
+  environment,
+  filters,
+  isRefreshVisible,
+  ...props
+}: Props) {
+  const { settings } = useTableSettings<ContainersTableSettings>();
+
+  const containersQuery = useContainers(
+    environment.Id,
+    true,
+    filters,
+    isRefreshVisible ? settings.autoRefreshRate * 1000 : undefined
+  );
+
+  if (!containersQuery.data) {
+    return null;
+  }
+
+  return (
+    <ContainersDatatable
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...props}
+      containers={containersQuery.data}
+      isRefreshVisible={isRefreshVisible}
+      environment={environment}
+    />
   );
 }
 
